d848e1d9ac1052d2ca309bb3815507b2778e9882
mary2018Deutschland <mary2018@ukr.net>
2024-10-10 10:52:18 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//https://www.figma.com/design/
+//RGuJf4CM436YyPOhXzLQCW/Untitled?node-id=1-2
+//&t=D2L5Bevd0ibyLcWq-0
+
+// ссылка на фигму
+
+import "./App.css";
+import { Spotify } from "./components/spotify";
+
+function App() {
+  return <Spotify />;
+}
+
+export default App;
